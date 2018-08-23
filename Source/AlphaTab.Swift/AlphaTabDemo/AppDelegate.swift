2e6275bee5b55b887171abd57d431296a3d5029e
Christian <christianmoler94@gmail.com>
2018-08-24 01:48:24 +0300

    Init demo app

---
+++ Source/AlphaTab.Swift/AlphaTabDemo/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  AlphaTabDemo
+//
+//  Created by Christian Moler on 24/08/2018.
+//  Copyright © 2018 Christian Moler. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+    
+}
+
