aea53376c2b95d7a66fe743a4bc76e9de1e8b91e
Christian <christianmoler94@gmail.com>
2018-08-24 01:44:25 +0300

    Init swift project

---
+++ Source/AlphaTab.Swift/AlphaTab.Swift/AlphaTab_Swift.h
@@ -0,0 +1,19 @@
+//
+//  AlphaTab_Swift.h
+//  AlphaTab.Swift
+//
+//  Created by Christian Moler on 24/08/2018.
+//  Copyright © 2018 Christian Moler. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AlphaTab_Swift.
+FOUNDATION_EXPORT double AlphaTab_SwiftVersionNumber;
+
+//! Project version string for AlphaTab_Swift.
+FOUNDATION_EXPORT const unsigned char AlphaTab_SwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AlphaTab_Swift/PublicHeader.h>
+
+
