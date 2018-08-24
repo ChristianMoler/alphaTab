75d15d7da0e0dd783e64a875872b7148c62c2f3c
Christian <christianmoler94@gmail.com>
2018-08-24 03:03:29 +0300

    Upd

---
+++ Source/AlphaTab.Swift/AlphaTab.Swift/Importer/GpxFileSystem.swift
@@ -0,0 +1,9 @@
+//
+//  GpxFileSystem.swift
+//  AlphaTab.Swift
+//
+//  Created by Christian Moler on 24/08/2018.
+//  Copyright © 2018 Christian Moler. All rights reserved.
+//
+
+import Foundation
---
+++ Source/AlphaTab.Swift/AlphaTab.Swift/Importer/GpxImporter.swift
@@ -0,0 +1,10 @@
+//
+//  GpxImporter.swift
+//  AlphaTab.Swift
+//
+//  Created by Christian Moler on 24/08/2018.
+//  Copyright © 2018 Christian Moler. All rights reserved.
+//
+
+import Foundation
+
